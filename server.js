a8aa3a447c5f7dcb070753ca3bf48d8526e1a7fe
Tytula Oleksandr <alekstitula84@gmail.com>
2023-07-22 12:07:08 +0300

    refact

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const { DB_HOST } = process.env;
-const { PORT } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log("Database connection successful on port", `${PORT}`);
+    console.log("Database connection successful");
     app.listen(3000);
   })
   .catch((err) => {
